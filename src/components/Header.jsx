6385d77267ec2363497510ffc1b26d1366fb8384
Mykhailo_Korzh <mishakorzh3@gmail.com>
2025-01-20 01:59:43 +0200

    updated styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,44 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { getUserEmail } from './CallbackHandler'; // Импорт функции для получения email
-import './Header.css';
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { getUserEmail, getAuthToken } from "./CallbackHandler";
+import "./Header.css";
 
 const Header = () => {
-  const userEmail = getUserEmail(); // Получаем email из sessionStorage
+  const navigate = useNavigate();
+  const userEmail = getUserEmail();
+  const authToken = getAuthToken();
+
+  const handleLogout = () => {
+    sessionStorage.removeItem("authToken");
+    sessionStorage.removeItem("userEmail");
+
+    window.location.href = "https://e3f1-185-223-114-81.ngrok-free.app/auth";
+  };
 
   return (
     <header className="header">
       <div className="header-container">
-        <div className="header-logo">
-          <img src={require('../assets/logo.png')} alt="Logo" />
+        <div className="header-left">
+          <div className="header-logo">
+            <img src={require("../assets/logo.png")} alt="Logo" />
+          </div>
+          <h1 className="header-title">AUK Quiz Generator</h1>
         </div>
-
-        <h1 className="header-title">AUK Quiz Generator</h1>
-
         <nav className="header-nav">
           <Link to="/quizzes" className="header-link">Quiz List</Link>
           <Link to="/materials" className="header-link">Materials</Link>
         </nav>
-
-        {/* Отображение email пользователя, если он доступен */}
-        {userEmail && <div className="header-user-email">{userEmail}</div>}
+        <div className="header-right">
+          {userEmail && <div className="header-user-email">{userEmail}</div>}
+          {(userEmail || authToken) && (
+            <button className="header-logout-button" onClick={handleLogout}>
+              Выйти
+            </button>
+          )}
+        </div>
       </div>
     </header>
+
   );
 };
 
